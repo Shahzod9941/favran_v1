0924eab2002691e7229ea9aaa2455fa5f212ee2a
Shahzod01 <r7072212@gmail.com>
2021-06-30 17:31:11 +0500

    banner ready with comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,84 @@
+
+
+new Swiper(".swiper-container", {
+    loop: true,
+    sapceBetween: 20,
+    slidesPerView: 6,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+                  200: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 0,
+                  },
+                  321: {
+                      slidesPerView: 2,
+                      spaceBetween: 5,
+                  },
+                  480: {
+                    slidesPerView: 2.7,
+                    spaceBetween: 10,
+                  },
+                  767: {
+                    slidesPerView: 4,
+                    spaceBetween: 5,
+                  },
+                  1200: {
+                    slidesPerView: 6,
+                    spaceBetween: 15,
+                  },
+                 },
+});
+
+let swiperBanner = new Swiper(".swiper-banner", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+// new Swiper(".swiper-container", {
+//     loop: true,
+//     slidesPerView: 6,
+  
+//     spaceBetween: 20,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: {
+//       200: {
+//         slidesPerView: 2.6,
+//         spaceBetween: 20,
+//       },
+//       540: {
+//         slidesPerView: 3,
+//         spaceBetween: 20,
+//       },
+//       767: {
+//         slidesPerView: 4,
+//         spaceBetween: 20,
+//       },
+//       1024: {
+//         slidesPerView: 5,
+//         spaceBetween: 20,
+//       },
+//       1200: {
+//         slidesPerView: 6,
+//         spaceBetween: 20,
+//       },
+//      },
+//   });
+
+
 document.addEventListener("DOMContentLoaded", ()=>{
     const nav = document.querySelector(".nav");
     document.querySelector("#btnNav").addEventListener("click", () =>{
